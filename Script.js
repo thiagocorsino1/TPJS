6582a34e38cfecad2382cac03819179f89d8d707
thiagocorsino1 <corsinothiago1@gmail.com>
2022-10-06 17:12:33 -0300

    commit

--- Script.js
+++ Script.js
@@ -1,3 +1,4 @@
+//#region
 /* Imprimir por consola los siguientes datos: variables, let, constantes. */
 var nombre = `thiago`;
 let fruta = `banana`;
@@ -7,26 +8,35 @@ console.log(nombre);
 console.log(fruta);
 console.log(hello);
 
+//#endregion
+
+//#region
 /* Generar funciones de cálculos matemáticos*/
 var num = 5;
 
 function square(num) {
-    return num * num;
+    return (num * num);
 }
+console.log(square)
 
 var num2 = 10;
 
 function suma(num2) {
     return num2 + num2;
 }
+console.log(suma);
 
 var num3 = 20;
 
 function division(num3) {
     return num3 / num3;
 }
+
+//#endregion
+//#region
 /* Generar un tipo de array y recorrerlo*/
 
 let estaciones = ["verano", "otoño", "invierno"];
 let [a, b, c] = estaciones;
 console.log(a, b, c);
+//#endregion
